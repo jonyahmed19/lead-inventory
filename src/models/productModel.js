c03d7068c1fc9e8f4ae8f02ad906edfe35536c09
Sahidul Islam <jonyahmed19@gmail.com>
2022-10-14 21:02:19 +0600

    added Product Schema

---
+++ src/models/productModel.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const validator = require("validator/es");
+const {ObjectId} = mongoose.Schema.Types;
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for this product"],
+        trim: true,
+        unique: [true, 'Name must be unique'],
+        lowerCase: true,
+        minLength: [3, 'Name must be at least 3 characters'],
+        maxLength: [100, 'Name is too large'],
+    },
+    description: {
+        type: String,
+        required: true
+},
+    unit: {
+        type: String,
+        required: true,
+        enum: {
+            values: ['kg', 'litre', 'pcs', 'bag'],
+            message: 'Unit value can\'t be {VALUE}, must be kg/litre/pcs/bag'
+        }
+    },
+    imageURLs: [{
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => {
+                if(!Array.isArray(value)){
+                    return false;
+                }
+                let isValid = true;
+                value.forEach(url => {
+                    if(!validator.isURL(url)){
+                        isValid = false;
+                    }
+                })
+                return  isValid;
+            }
+        },
+        message: "Please provide valid image urls"
+    }],
+    category: {
+        type: String,
+        required: true
+    },
+    brand : {
+        name: {
+            type: String,
+            required: true,
+        },
+        id: {
+            type: ObjectId,
+            ref: 'Brand',
+            required: true
+        }
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+productSchema.pre('save', function (next){
+
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
